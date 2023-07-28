0db5e9efdb6c0dc3fcff03416d3294bfadb678dc
feduke-nukem <feduke-nukem@yandex-team.ru>
2023-07-29 01:41:09 +0400

    init

---
+++ android/app/src/main/kotlin/com/platform/practice/to_image_sync_leak/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.platform.practice.to_image_sync_leak
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/scheduler.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  final _key = GlobalKey();
+  late final _ticker = createTicker((elapsed) => _rasterizeSync());
+
+  @override
+  void initState() {
+    super.initState();
+
+    SchedulerBinding.instance.addPostFrameCallback((_) {
+      _ticker.start();
+    });
+  }
+
+  @override
+  void dispose() {
+    _ticker.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RepaintBoundary(
+      key: _key,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'To image sync leak',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _rasterizeSync() async {
+    final image =
+        (_key.currentContext!.findRenderObject() as RenderRepaintBoundary)
+            .toImageSync();
+    image.dispose();
+  }
+}
